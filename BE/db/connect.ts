9813c96b1845ce2e8934ad4d00ea02007c33fadd
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-09-06 14:42:41 +0300

    rename db

--- BE/db/connect.ts
+++ BE/db/connect.ts
@@ -3,8 +3,9 @@ import { Logger } from '../helpers/logger'
 
 const MONGO_PWD = process.env.MONGO_PWD ? process.env.MONGO_PWD : ''
 const MONGO_USERNAME = process.env.MONGO_USERNAME ? process.env.MONGO_USERNAME : ''
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME ? process.env.MONGO_DB_NAME : ''
 
-const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PWD}@cluster-qe6qzxn4pjhqk0f.ta2sn.mongodb.net/?retryWrites=true&w=majority&appName=cluster-QE6QzxN4pjhqK0F86Xsm`
+const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PWD}@cluster-qe6qzxn4pjhqk0f.ta2sn.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority&appName=cluster-QE6QzxN4pjhqK0F86Xsm`
 
 export const connectDB = async () => {
     /** Connect to MongoDB **/
