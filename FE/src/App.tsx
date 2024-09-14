a49d1f8f0251c9f641b10f933cb94a09e7499909
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-09-14 14:34:37 +0300

    fix

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,5 +1,5 @@
-import { lazy, useEffect } from 'react'
 import { Loader } from 'lucide-react'
+import { lazy, useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
