1da97bb068c988a1f261b507dbdd1d473ff5d525
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-09-14 13:01:07 +0300

    add favicon

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import App from './App.tsx'
 
@@ -9,7 +10,14 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <HelmetProvider>
+                <Helmet>
+                    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                    <title>{'[my-app]MERN Stack'}</title>
+                </Helmet>
+
+                <App />
+            </HelmetProvider>
         </BrowserRouter>
     </StrictMode>
 )
