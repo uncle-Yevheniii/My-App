b7fda8e4c2702f47bb2d1c2f8b302c4e52b3aa9c
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-09-15 12:50:33 +0300

    deploy

--- BE/index.ts
+++ BE/index.ts
@@ -1,5 +1,6 @@
 import http from 'http'
 import cors from 'cors'
+import path from 'path'
 import dotenv from 'dotenv'
 import express from 'express'
 import cookieParser from 'cookie-parser'
@@ -11,9 +12,21 @@ import { Logger } from './helpers/logger'
 dotenv.config()
 
 const app = express()
+
+const __dirname = path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+
+/** Static Server **/
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/FE', 'dist')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/FE', 'dist', 'index.html'))
+    })
+}
 
 /** Routes **/
 const prefix = '/api'
--- FE/src/pages/PageDashboard.tsx
+++ FE/src/pages/PageDashboard.tsx
@@ -63,9 +63,7 @@ export default function DashboardPage() {
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.95 }}
                     onClick={handleLogout}
-                    className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
-				font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700
-				 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+                    className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
                 >
                     Logout
                 </motion.button>
