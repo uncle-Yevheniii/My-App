1e1606380fabc15c0dd644c7ac91d8e0c790baa7
Yevgenii <yevhenii.katsimon@gmail.com>
2024-10-10 23:36:04 +0300

    about text

--- FE/src/pages/PageAboutProject.tsx
+++ FE/src/pages/PageAboutProject.tsx
@@ -4,15 +4,15 @@ export default function AboutProjectPage() {
             <h2 className="title-text">About Project</h2>
             <div className="bg-secondary p-8 rounded-2xl mb-5">
                 <p className="text-xl text-background font-normal">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptatum atque officiis accusamus, velit voluptas iusto consectetur
-                    est quis totam aliquid deleniti, nisi nulla repellat autem amet. Nemo quos dolorem id officia quibusdam aliquam aperiam vero,
-                    beatae delectus, rem vel. Quos magnam tempore doloribus eum explicabo officia temporibus maiores vero?
+                    This is my pet project, the essence of which is user registration, verification and authorization. <br />
+                    <br />
+                    The project was written on TS using the following technologies: Axios, React, Redux, Node, Express, Mongoose and JWT.
                     <br />
                     <br />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptatum atque officiis accusamus, velit voluptas iusto consectetur
-                    est quis totam aliquid deleniti, nisi nulla repellat autem amet. Nemo quos dolorem id officia quibusdam aliquam aperiam vero,
-                    beatae delectus, rem vel. Quos magnam tempore doloribus eum explicabo officia temporibus maiores vero?
+                    Implemented: [Registration of a new user, Email verification, Uploading a profile photo, Exiting the profile, Logging in using
+                    user data and Receiving data via cookies].
                     <br />
+                    Data validation is present on the registration and login forms. Validation is present both on the server and on the client
                 </p>
             </div>
         </section>
